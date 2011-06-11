e4e1ecb8e126d89fd7cd5e70e02e8490bab86751
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-06-11 16:49:03 +0200

    Added an example for the Recording module (does not compile).

---
+++ examples/recording.ml
@@ -0,0 +1,34 @@
+(* Demonstrate the use of recording surfaces.  *)
+
+open Printf
+
+let () =
+  let extents = { Cairo.x = 0.0; y = 0.0; w = 120.0; h = 120.0 } in
+  let surface = Cairo.Recording.create ~extents Cairo.COLOR in
+  let cr = Cairo.create surface in
+
+  (* Drawing code goes here *)
+  Cairo.set_line_width cr 10.;
+  Cairo.set_source_rgb cr 0. 0. 0.;
+
+  Cairo.move_to cr 25. 25.;
+  Cairo.line_to cr 120. 120.;
+  Cairo.stroke cr;
+  let r = Cairo.Recording.ink_extents surface in
+  printf "extents = {x=%g; y=%g; w=%g; h=%g}\n"
+    r.Cairo.x r.Cairo.y r.Cairo.w r.Cairo.h;
+
+  (* Replay the recorded content to a PNG and PDF output *)
+  (* PNG *)
+  let png_surface = Cairo.Image.create Cairo.Image.ARGB32 120 120 in
+  let png_cr = Cairo.create png_surface in
+  Cairo.set_source_surface png_cr surface 0.0 0.0;
+  Cairo.paint png_cr;
+  Cairo.PNG.write png_surface "recording.png";
+  (* PDF *)
+  let pdf_surface = Cairo.PDF.create "recording.pdf" 120.0 120.0 in
+  let pdf_cr = Cairo.create pdf_surface in
+  Cairo.set_source_surface pdf_cr surface 0.0 0.0;
+  Cairo.paint pdf_cr;
+  Cairo.Surface.finish (Cairo.get_target pdf_cr)
+
