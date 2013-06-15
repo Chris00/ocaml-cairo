5b1f7db9f7d5176dd4ffadd40f79ed7d9d8c4a1f
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-06-15 11:42:27 +0200

    Make sure setup.ml works with OPAM.

--- setup.ml
+++ setup.ml
@@ -1,3 +1,7 @@
+let () =
+  try Topdirs.dir_directory (Sys.getenv "OCAML_TOPLEVEL_PATH")
+  with Not_found -> ();;
+
 (* OASIS_START *)
 (* DO NOT EDIT (digest: 7f47a529f70709161149c201ccd90f0b) *)
 #use "topfind";;
