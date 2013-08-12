90462dfb2044c604437b71ebf9f6636c37758b29
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-08-12 18:12:31 +0200

    Add test about writing on a surface after it is finished.
    Displays a Cairo bug.

---
+++ tests/test_finish.ml
@@ -0,0 +1,18 @@
+open Printf
+
+let () =
+  let surface = Cairo.Image.(create ARGB32 100 100) in
+  let ctx = Cairo.create surface in
+
+  Cairo.set_line_width ctx 1.;
+  Cairo.move_to ctx 0. 0.;
+  Cairo.line_to ctx 100. 100.;
+  Cairo.stroke ctx;
+  Cairo.Surface.finish surface;
+  Cairo.line_to ctx 100. 0.;
+  (* The following command should raise Error(SURFACE_FINISHED) but
+     does not currently because of a bug in Cairo
+     https://bugs.freedesktop.org/show_bug.cgi?id=68014 *)
+  try Cairo.stroke ctx
+  with Cairo.Error(Cairo.SURFACE_FINISHED) ->
+    printf "OK, correct exception raised\n"
