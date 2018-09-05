9c93906172280b6ee406e748538d51177ca530ff
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2018-09-05 20:41:37 +0200

    Update Travis script

--- config.ml
+++
@@ -1,8 +0,0 @@
-(* Configuration to e.g. specify the path to the Cairo library. *)
-
-(* Complete CFLAGS and CLIBS if the default values do not work. *)
-
-let cairo_cflags = [] (* e.g. ["-I/usr/include/cairo"] *)
-let cairo_clibs  = [] (* e.g. ["-L/usr/lib";  "-lcairo"] *)
-
-let gtk_cflags = []
